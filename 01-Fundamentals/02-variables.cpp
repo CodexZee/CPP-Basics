8698172cadcefda30725c92d689dbd49e4265a3f
Rao Zeeshan Ul Haq <314577677+CodexZee@users.noreply.github.com>
2026-08-09 23:53:31 +0500

    Add variables example in C++

---
+++ 01-Fundamentals/02-variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 33;
+    float height = 5.7;
+    char grade = 'A';
+    bool learning = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Learning C++: " << learning << endl;
+
+    return 0;
+}
